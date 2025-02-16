eabf5f49b9193bcfd7e7abd0df36c6e776c58192
Cakkii <Icecream@rushtonpash.com>
2025-02-16 13:29:24 +1100

    Implemented basic timeline

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import { Grid2 as Grid, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { CafeBlog, CafeBlogData } from "./components/CafeBlog.tsx";
 import { CoffeeBlog, CoffeeBlogData } from "./components/CoffeeBlog.tsx";
+import CoffeeIcon from "@mui/icons-material/Coffee";
+import FlareIcon from "@mui/icons-material/Flare";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 
 function App() {
   const [password, setPassword] = useState("");
@@ -84,19 +93,49 @@ function App() {
             </Button>
           </div>
           <div>
+            <Timeline position="alternate">
+              {data.map((blog, index) => (
+                <TimelineItem
+                  key={`${isCafe(blog) ? blog.cafeName : blog.coffeeName}-${index}`}
+                >
+                  <TimelineOppositeContent sx={{ py: "12px", px: 2 }}>
+                    <Typography variant="h6" component="span">
+                      {isCafe(blog) ? blog.cafeName : blog.coffeeName}
+                    </Typography>
+                    <Typography>
+                      {isCafe(blog) ? blog.order : blog.coffeeType}
+                    </Typography>
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary">
+                      {isCafe(blog) ? (
+                        <FlareIcon sx={{ color: "#39e75f" }} />
+                      ) : (
+                        <CoffeeIcon sx={{ color: "#563618" }} />
+                      )}
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent color="text.secondary" sx={{ m: "auto 0" }}>
+                    {new Date(blog.date).toLocaleDateString()}
+                  </TimelineContent>
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </div>
+          <div>
             {data.map((item, index) => (
               <div>
                 {isCafe(item) && (
                   <CafeBlog key={`${item.cafeName}-${index}`} {...item} />
                 )}
                 {isCoffee(item) && (
-                  <CoffeeBlog key={"${item.coffeeName}-${index}"} {...item} />
+                  <CoffeeBlog key={`${item.coffeeName}-${index}`} {...item} />
                 )}
               </div>
             ))}
           </div>
         </Grid>
-        <Grid size={1}></Grid>
         <Grid size={12}>
           <Typography variant="body1">
             This is the footer and a second test commit!
